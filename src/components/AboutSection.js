cef1aec24073b42ea9f87221dd1ba80e074ade6e
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2020-12-04 14:34:05 +0700

    Intro to styled component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,68 @@
-import home1 from '../img/home1.png'
+import home1 from "../img/home1.png";
+
+// Stlyed
+import styled from "styled-components";
+
 const AboutSection = () => {
   return (
-    <div>
-      <div className='description'>
-        <div className='title'>
-          <div className='hide'>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have a professional with amazing skills to help you achieve it.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className='image'>
-        <img src={home1} alt='guys with something' />
-      </div>
-    </div>
-  )
-}
+      </Description>
+      <Image>
+        <img src={home1} alt="guys with something" />
+      </Image>
+    </About>
+  );
+};
 
-export default AboutSection
+// Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+export default AboutSection;
