24edfd026af76275aa3032d7e891249c2f1f6a05
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2020-12-04 10:41:19 +0700

    FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How Do I Start?</h4>
+        <div className='answer'>
+          <p>Nulla commodo amet mollit veniam in mollit mollit ut.</p>
+          <p>
+            Cillum anim sit amet aliqua dolore. Excepteur excepteur minim ea eu
+            pariatur in sunt tempor adipisicing id officia sunt ullamco culpa.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Daily Schedule</h4>
+        <div className='answer'>
+          <p>Nulla commodo amet mollit veniam in mollit mollit ut.</p>
+          <p>
+            Cillum anim sit amet aliqua dolore. Excepteur excepteur minim ea eu
+            pariatur in sunt tempor adipisicing id officia sunt ullamco culpa.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Different Payment Method</h4>
+        <div className='answer'>
+          <p>Nulla commodo amet mollit veniam in mollit mollit ut.</p>
+          <p>
+            Cillum anim sit amet aliqua dolore. Excepteur excepteur minim ea eu
+            pariatur in sunt tempor adipisicing id officia sunt ullamco culpa.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>What Product do you offer</h4>
+        <div className='answer'>
+          <p>Nulla commodo amet mollit veniam in mollit mollit ut.</p>
+          <p>
+            Cillum anim sit amet aliqua dolore. Excepteur excepteur minim ea eu
+            pariatur in sunt tempor adipisicing id officia sunt ullamco culpa.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FaqSection
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import AboutSection from '../components/AboutSection'
 import ServiceSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServiceSection />
+      <FaqSection />
     </>
   )
 }
