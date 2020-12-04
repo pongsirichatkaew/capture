9036ca87e8a06ed7a8603695117cb1d8b1f75947
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2020-12-04 10:24:51 +0700

    ServiceSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,12 @@
 import AboutSection from '../components/AboutSection'
+import ServiceSection from '../components/ServicesSection'
 const AboutUs = () => {
-  return <AboutSection />
+  return (
+    <>
+      <AboutSection />
+      <ServiceSection />
+    </>
+  )
 }
 
 export default AboutUs
